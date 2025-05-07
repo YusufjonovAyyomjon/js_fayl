7ec7ae9b1380968f06b600ee55afaacb2bd91ce3
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-05-07 21:52:00 +0500

    ishladi

---
+++ homwork/1-dars/index.js
@@ -0,0 +1,8 @@
+var fullName = " var da cod larimiz aralashib ketadi"
+.console.log(fullName);
+
+let fullName2 = "letda cod larimiz aralshib ketmaydi va eror beradi"
+console.log(fullName2)
+
+const fullName3 = "const da bitta qiymat yoki textlar ga bitta name beriladi agar ushbu qiymatlarga 2 ta name biriktrsa eror beradi"
+console.assert(fullName3)
---
+++ lassens/1-dars/index.js
@@ -0,0 +1,16 @@
+// var namber = 149;
+// var namber2 = 482;
+// console.log(namber + namber2);
+
+// let fullName = "yusufjonov ayyomjon"
+// console.log(fullName)
+
+// const NAME = "Oxford"
+// const Name = "O'quv markazi"
+// console.log(NAME + Name)
+
+let namber = 19;
+let namber2 = 9;
+let namber3 = 5;
+let namber4 = 12;
+console.log(namber4+(namber-namber2)*namber3 )
