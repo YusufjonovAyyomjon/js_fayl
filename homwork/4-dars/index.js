203ec28c71d3fcab8b250d89e2b377b1508f7e1c
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-05-10 14:08:09 +0500

    ishladi

---
+++ homwork/4-dars/index.js
@@ -0,0 +1,48 @@
+// let a = 2;
+// let b = 2;
+
+// a + b < 4 ?console.log("Below") : console.log("Over");
+
+// let a = prompt('0 dan 3 gacha son kiriting','');
+// switch (a) {
+//     case '0':
+//         console.log("0");
+//         break;
+//     case "1" :
+//         console.log("1");
+//         break;    
+
+//     case "2" && "3" :
+//      console.log('2,3');
+//         break;
+//     default:
+//         console.log("siz noto'g'ri raqam kirittingiz");
+// }
+
+
+// let a = +prompt("a?")
+// switch (a) {
+//   case 0:
+//     console.log(0)
+//     break;
+//   case 1:
+//   console.log(1)
+//     break;
+//     case 2||3:
+//       console.log("2,3")
+  
+//   default:
+//   console.log('ishlamadi')
+// }
+
+// let browers = prompt("browers ni kiriting")
+// if ('edge' == browers) {
+//   console.log("You've got the Edge! ")
+// }
+// else if ("chrome" == browers || "safari" == browers || "opera" == browers) {
+//   console.log("Okay we support these browsers too")
+// }else {
+//   console.log("We hope that this page looks ok!")
+// }
+let message ;
+
