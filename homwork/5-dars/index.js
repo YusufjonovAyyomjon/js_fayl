a1296966feed422e49c153ee65b1650653a15e08
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-05-13 01:32:59 +0500

    ishlade

---
+++ homwork/5-dars/index.js
@@ -0,0 +1,134 @@
+
+// let a = 7, b = 7, c = 0;
+
+// let raqam = 0;
+
+// if (a >= 1) { 
+//     raqam++; 
+
+// }
+// if (b >= 1) { 
+//     raqam++; 
+// }
+// if (c >= 1) { 
+//     raqam++; 
+// }
+
+// console.log(raqam);
+
+
+// let d = 7, f = 1, x = -4;
+
+// let raqam1 = 0;
+// let raqam2 = 0;
+
+// if (d >= 1) { 
+//     raqam1++; 
+
+// }
+// else if (d <= -1) { 
+//     raqam2++; 
+
+// }
+// else {
+//     console.log("0 dan ktta yoki kichik son kiriting");
+    
+// }
+
+// if (f >= 1) { 
+//     raqam1++; 
+// }
+
+// else if (f <= -1) { 
+//     raqam2++; 
+// }
+// else{
+//     console.log("0 dan ktta yoki kichik son kiriting");
+// }
+// if (x >= 1) { 
+//     raqam1++; 
+// }
+
+// else if (x <= -1) { 
+//     raqam2++; 
+// }
+// else{
+//     console.log("0 dan ktta yoki kichik son kiriting");
+// }
+
+// console.log(raqam1,raqam2);
+
+
+
+// let a = 5, b = 6;
+// let num = 0;
+// if (a>b) {
+//     num = a
+//     num =b
+//     console.log(`shu son ${a} bunisidan katta ${b}`);
+    
+// }
+// if (a<b) {
+//     num = a
+//     num = b
+//     console.log(`shu son ${b} bunisidan katta ${a}`);
+    
+// }
+// console.log(num);
+
+
+
+// let a = 5, b = 4;
+// let num3 = 0;
+// if (a<b) {
+//     num3 = a
+//     console.log(`bu soning tartibraqami  ${a}`);
+    
+// }
+// if (a>b) {
+//     num3 = b
+//     console.log(`bu soning tartibraqami  ${b}`);
+    
+// }
+// console.log(num3);
+
+
+// let a = 9, b = 6;
+// if (a>b) {
+//     console.log(`shu son katta ${a} bu son kickina ${b}`);
+    
+// }
+// if (a<b) {
+//     console.log(`shu son katta ${b} bu son kichkina ${a}`);
+    
+// }
+// console.log(a,b);
+
+
+let a = 5, b = 1, c = 2;
+let num = 0;
+if (a<b<c) {
+    num = a
+    num = b
+    num = c
+    
+}
+
+if (a<b<c) {
+    num = a
+    num = b
+    num = c
+    
+}
+
+
+
+if (a<b<c) {
+    num = a
+    num = b
+    num = c
+}
+
+    
+
+console.log(num);
---
+++ lassens/5-dars/index.js
@@ -0,0 +1,14 @@
+let number = -1;
+
+if (number < 0){
+    number = number - 2; 
+}
+    else if(number > 0){
+        
+        ++number; 
+    }
+    else{
+        console.log("siz 0 dan katta yoki kikchik kiriting");
+    
+}
+console.log(number);
