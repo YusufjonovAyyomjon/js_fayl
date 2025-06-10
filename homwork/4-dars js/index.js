7762c51b8c6ee4a2c2f6f736c60751a47070ab9b
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-06-10 08:57:26 +0500

    chenged

---
+++ homwork/4-dars js/index.js
@@ -0,0 +1,112 @@
+// let result = ("a" + "b" < 4) ? console.log("ha katta") : console.log("yoq kichik");
+
+
+// =================================================
+
+
+// let browser = "Chrome"
+
+// if (browser === 'Edge') {
+//     console.log("Siz Edge brauzeridan foydalanyapsiz");
+// } else if (
+//     browser === 'Chrome' ||
+//     browser === 'Firefox' ||
+//     browser === 'Safari' ||
+//     browser === 'Opera'
+// ) {
+//     console.log('Bu brauzerlar sizdaham ishlaydi');
+// } else {
+//     console.log('xato');
+// }
+
+
+
+
+
+// ============================================================
+
+
+
+
+// let a = +prompt("a?","");
+// switch (a) {
+//     case 0:
+//         console.log("0");
+        
+//         break;
+//     case 1:
+//         console.log("1");
+        
+//         break;
+//     case 2:
+//     case 3:    
+//         console.log("2,3");
+        
+//         break;
+
+//     default:
+//         console.log("notog'ri son kirittingiz");
+        
+
+// }
+
+
+
+
+// ===================================================================
+
+// let login = "";
+// login === "employee" ? console.log("hello"):console.log("arrow");
+// login === "director" ? console.log("greetings"):console.log("arrow");
+// login === "" ? console.log("no login"):console.log("arrow");
+
+// ==================================================================
+
+
+let a = Number(prompt("31 yoshdan kichik yosh kiriting qayerda o'qishingizni aniqlab beramiz"));
+
+
+switch (a) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        console.log("bu bola boqchada oqiydi");
+        
+        break;
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+        console.log("bu bola maktabda oqiydi");
+        
+        break;
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+    case 25:
+    case 26:
+    case 27:
+    case 28:
+    case 29:
+    case 30:
+        console.log("bu bola universtet oqiydi");
+        
+        break;
+
+    default:
+        console.log("xato yosh kirittingiz");
+        
+}
