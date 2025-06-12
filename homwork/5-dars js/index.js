26b8da40ac2fecf9d6c3ca3de3d48e44ae3d4f03
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-06-12 13:55:55 +0500

    chenged

---
+++ homwork/5-dars js/index.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i < 5; ++i) {//1 dan boshlanib 4 da tugaydi chunki ++ belgisi ozgaruv chidan oldin  kelsa ozgaruv chini qadamini 1 ga oshirib keyin console.log ga chiqadi
+//     console.log(i);
+// }
+
+
+
+
+// for (let i = 0; i < 5; i++) { // 0 dan boshlanib 4 da tugaydi chunki ++ belgisi ozgaruv chidan keyinkelsa  aval ozgaruv chini chiqaradi keyin qoshadi
+//     console.log(i);
+// }
