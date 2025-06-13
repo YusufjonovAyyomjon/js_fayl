23411612d22cf81dc5a38597b42d080c7e3330c4
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-06-13 15:00:40 +0500

    chenged

---
+++ lassens/5-DARS JS/index.js
@@ -0,0 +1,40 @@
+// let a = 0;
+// for(;a <= 100 ; a++){
+//     if (a % 5 === 0 && a % 3 === 0 ) {
+//         console.log(a);
+        
+//     }
+// }
+
+// while(a <=100){
+//         if (a % 5 === 0) {
+//         console.log(a);
+        
+//     }
+//     a++
+
+// }
+
+// do {
+//        if (a % 5 === 0) {
+//         console.log(a);
+        
+//     }
+//     a++
+// }while(a<=100)
+
+
+// let a = 0
+// for (; a <= 100; a++){
+//     if(){
+//         console.log(a);
+        
+//     }
+// }
+
+// for(; ;){
+//     let i = prompt( `Qiymat kiriting`);
+//     let num = i === null ? i : Number(i);
+//     if(num > 100 || num === null) break;
+//     else continue;
+// }
