af9b0387306230a96d1974248ed1c24b601f6741
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-06-17 14:10:12 +0500

    ishladi

--- homwork/7-dars js/index.js
+++ homwork/7-dars js/index.js
@@ -1,39 +1,46 @@
 
-let d = 10, v = 2, q =4;
-function ortachaarifmetik (a,b,c){
-    let ortacha;
+// let d = 10, v = 2, q =4;
+// function ortachaarifmetik (a,b,c){
+//     let ortacha;
 
     
-    if(a>b && a<c){
-        console.log(a,"ortacha");
-        max=a
-    }
-    else if(b>a && b<c){
-        console.log(b,"ortacha");
-        ortacha=b
-    }
-    else if(c>a && c<b){
-        console.log(c,"ortacha");
-        ortacha=c
-    }
+//     if(a>b && a<c){
+//         console.log(a,"ortacha");
+//         max=a
+//     }
+//     else if(b>a && b<c){
+//         console.log(b,"ortacha");
+//         ortacha=b
+//     }
+//     else if(c>a && c<b){
+//         console.log(c,"ortacha");
+//         ortacha=c
+//     }
     
     
     
-    else if(a<b && a>c){
-        console.log(a,"ortacha");
-        ortacha=a
-    }
-    else if(b<a && b>c){
-        console.log(b,"ortacha");
-        ortacha=b
-    }
-    else if(c<a && c>b){
-        console.log(c,"ortacha");
-        ortacha=c
-    }
+//     else if(a<b && a>c){
+//         console.log(a,"ortacha");
+//         ortacha=a
+//     }
+//     else if(b<a && b>c){
+//         console.log(b,"ortacha");
+//         ortacha=b
+//     }
+//     else if(c<a && c>b){
+//         console.log(c,"ortacha");
+//         ortacha=c
+//     }
     
-}
+// }
 
-ortachaarifmetik(d,v,q)
+// ortachaarifmetik(d,v,q)
 
 
+// let a = 4,b=10,s=8;
+// let f = a+b+s;
+// console.log(f/3);
+
+// let a = 2,b=8,s=4;
+// let f = a*b*s;
+// console.log(f);
---
+++ lassens/4-DARS JS/index.js
@@ -0,0 +1,72 @@
+
+// let a = Number(prompt(" son kiriting:"));
+// // if (a % 2){
+//     //     console.log(`bu son toq ${a}` );
+    
+//     // }else{
+//         //     console.log(`bu son juft ${a}` );
+        
+//         // }
+// let a = Number(prompt("yilingizni kiriting:"));
+
+// let b = 2025 - a;
+// console.log(b);
+
+// if (b >= 2 && b <= 6){
+//     console.log("boqchada oqiysiz");
+    
+// }
+// else if (b >= 7 && b <= 18){
+//     console.log("maktabda oqiysiz");
+    
+// }
+// else if (b >= 19 && b <= 30){
+//     console.log("universtetda oqiysiz");
+    
+// }
+// else if (b >= 31 && b <= 60){
+//     console.log("ishlay sz");
+    
+// }
+// else if (b >= 61){
+//     console.log("nafaqaxo'r sz");
+    
+// }else{
+//     console.log("notog'ri yosh kirittingi");
+    
+// }
+
+let a = 10, b = 2, c =4;
+let ortacha;
+
+
+if(a>b && a<c){
+    console.log(a,"ortacha");
+    max=a
+}
+else if(b>a && b<c){
+    console.log(b,"ortacha");
+    ortacha=b
+}
+else if(c>a && c<b){
+    console.log(c,"ortacha");
+    ortacha=c
+}
+
+
+
+else if(a<b && a>c){
+    console.log(a,"ortacha");
+    ortacha=a
+}
+else if(b<a && b>c){
+    console.log(b,"ortacha");
+    ortacha=b
+}
+else if(c<a && c>b){
+    console.log(c,"ortacha");
+    ortacha=c
+}
+
+console.log(ortacha);
+
