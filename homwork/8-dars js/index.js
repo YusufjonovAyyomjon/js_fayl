6523d6a7199189d92fda5e84953f6e7a71366d30
YusufjonovAyyomjon <yusufjonovayyomjon@gmail.com>
2025-06-24 10:15:46 +0500

    chenged

---
+++ homwork/8-dars js/index.js
@@ -0,0 +1,135 @@
+
+// let min;
+
+// function minimum (a,b){
+//     if (a < b) {
+//         console.log(a, "minimum");
+        
+//         min=a
+//     }
+//     if (b < a) {
+//         console.log(b, "minimum");
+//        min=b
+//     }
+// }
+// minimum(2,4)// javob: 2 chiqadi
+
+// let min;
+
+// function minimum (a,b){
+//     if (a < b) {
+//         console.log(a, "minimum");
+        
+//         min=a
+//     }
+//     if (b < a) {
+//         console.log(b, "minimum");
+//        min=b
+//     }
+// }
+// minimum(-1,3)// javob: -1 chiqadi
+
+// let min;
+// let teng;
+
+// function minimum (a,b){
+//     if (a < b ) {
+//         console.log(a, "minimum");
+        
+//         min=a
+//     }
+//     if (b < a) {
+//         console.log(b, "minimum");
+//        min=b
+//     }
+//     if (a == b ) {
+//         console.log(a, "teng");
+        
+//         teng=a
+//     }
+
+// }
+// minimum(1,1)// javob: 1 chiqadi
+
+
+
+
+
+
+
+
+
+
+// ==========================================
+
+// let a = 3;
+// let b = 2;
+// function daraja (daraja,daraja2){
+//     let newdrj = daraja ** daraja2;
+//     console.log(newdrj);
+    
+// }
+// daraja(a,b) // javob: 9
+
+// let a = 3;
+// let b = 3;
+// function daraja (daraja,daraja2){
+//     let newdrj = daraja ** daraja2;
+//     console.log(newdrj);
+    
+// }
+// daraja(a,b)// javob: 27
+
+// let a = 1;
+// let b = 100;
+// function daraja (daraja,daraja2){
+//     let newdrj = daraja ** daraja2;
+//     console.log(newdrj);
+    
+// }
+// daraja(a,b)// javob: 1
+
+
+
+// ================================================
+
+
+// let ask = (question,yes,no)=>{
+    //     if (confirm(question)) yes();
+    //     else no();
+    // }
+    
+    // ask(
+        //     "Do you agree?",
+        //     function(){console.log("you agreed.");},
+        //     function(){console.log("you canceled the execution");}
+        // )
+        
+        
+// ================================================
+
+
+
+
+// let student = {
+//     firstName: "Eshmat",
+//     lastName: "Toshmatov",
+//     age: 20,
+//     course: "Frontend"
+//   };
+
+// console.log(student.firstName,student.lastName,student.age);// console.log ga eshmat toshmatov 20 chiqadi
+
+
+
+// let data = {
+//     name: "Phone",
+//     model: "iPhone",
+//     price: 700,
+//     quantity : 8,
+//   };
+
+// let newdata = data.price * data.quantity;
+// console.log("umumiy hisob",newdata); // javob: 5600 chiqadi
+  
+  
